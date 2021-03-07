7f39d6dab5cd6ab7906989a932dadb41ac687f07
Aung Maw <aungmawjj12@gmail.com>
2021-03-07 21:06:42 +0800

    [merkle] added tree helpers

--- merkle/tree.go
+++ merkle/tree.go
@@ -1,6 +1,9 @@
 package merkle
 
-import "math/big"
+import (
+	"crypto"
+	"math/big"
+)
 
 // Position of a node in the tree
 type Position struct {
@@ -10,5 +13,58 @@ type Position struct {
 
 // Bytes binary value of a position
 func (p *Position) Bytes() []byte {
-	return append([]byte{p.Level}, p.Index.Bytes()...)
+	ib := p.Index.Bytes()
+	b := make([]byte, 0, 1+len(ib))
+	b = append(b, p.Level)
+	b = append(b, ib...)
+	return b
+}
+
+// TreeOptions type
+type TreeOptions struct {
+	BranchFactor uint8
+	HashFunc     crypto.Hash
+}
+
+// Tree type
+type Tree struct {
+	bfactor *big.Int
+}
+
+// NewTree creates a new Merkle Tree
+func NewTree(opts TreeOptions) *Tree {
+	t := new(Tree)
+	if opts.BranchFactor < 2 {
+		opts.BranchFactor = 2
+	}
+	t.bfactor = big.NewInt(int64(opts.BranchFactor))
+	return t
+}
+
+func (t *Tree) blockCount(nodeCount *big.Int) *big.Int {
+	// ceil(nodeCount / bfactor)
+	count := big.NewInt(0)
+	m := big.NewInt(0)
+	count.DivMod(nodeCount, t.bfactor, m)
+	if m.Cmp(big.NewInt(0)) == 1 {
+		count.Add(count, big.NewInt(1))
+	}
+	return count
+}
+
+func (t Tree) firstNodeInBlock(blkIdx *big.Int) *big.Int {
+	idx := big.NewInt(0)
+	return idx.Mul(blkIdx, t.bfactor)
+}
+
+func (t *Tree) blockOfNode(nodeIdx *big.Int) *big.Int {
+	// floor(nodeIdx / bfactor)
+	idx := big.NewInt(0)
+	return idx.Div(nodeIdx, t.bfactor)
+}
+
+func (t *Tree) nodeIndexInBlock(nodeIdx *big.Int) *big.Int {
+	// mod(nodeIdx / bfactor)
+	idx := big.NewInt(0)
+	return idx.Mod(nodeIdx, t.bfactor)
 }
--- merkle/tree_test.go
+++ merkle/tree_test.go
@@ -1,6 +1,7 @@
 package merkle
 
 import (
+	"crypto"
 	"math/big"
 	"testing"
 
@@ -23,6 +24,105 @@ func TestPosition_Bytes(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			p := &Position{tt.level, tt.index}
 			assert.EqualValues(t, tt.want, p.Bytes())
+		})
+	}
+}
+
+func TestNewTree(t *testing.T) {
+	tests := []struct {
+		name string
+		opts TreeOptions
+		want *big.Int
+	}{
+		{"branch factor < 2", TreeOptions{1, crypto.SHA1}, big.NewInt(2)},
+		{"branch factor >= 2", TreeOptions{4, crypto.SHA1}, big.NewInt(4)},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tree := NewTree(tt.opts)
+			assert.Equal(t, 0, tt.want.Cmp(tree.bfactor))
+		})
+	}
+}
+
+func TestTree_blockCount(t *testing.T) {
+	tests := []struct {
+		name      string
+		bfactor   uint8
+		nodeCount *big.Int
+		want      *big.Int
+	}{
+		{"one node", 4, big.NewInt(1), big.NewInt(1)},
+		{"no remainder", 4, big.NewInt(8), big.NewInt(2)},
+		{"has remainder", 4, big.NewInt(10), big.NewInt(3)},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tree := NewTree(TreeOptions{tt.bfactor, crypto.SHA1})
+			got := tree.blockCount(tt.nodeCount)
+			assert.Equal(t, 0, tt.want.Cmp(got))
+		})
+	}
+}
+
+func TestTree_firstNodeInBlock(t *testing.T) {
+	tests := []struct {
+		name    string
+		bfactor uint8
+		blkIdx  *big.Int
+		want    *big.Int
+	}{
+		{"block zero", 4, big.NewInt(0), big.NewInt(0)},
+		{"block 1", 4, big.NewInt(1), big.NewInt(4)},
+		{"block 2", 4, big.NewInt(2), big.NewInt(8)},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tree := NewTree(TreeOptions{tt.bfactor, crypto.SHA1})
+			got := tree.firstNodeInBlock(tt.blkIdx)
+			assert.Equal(t, 0, tt.want.Cmp(got))
+		})
+	}
+}
+
+func TestTree_blockOfNode(t *testing.T) {
+	tests := []struct {
+		name    string
+		bfactor uint8
+		nodeIdx *big.Int
+		want    *big.Int
+	}{
+		{"node zero", 4, big.NewInt(0), big.NewInt(0)},
+		{"first node in block 1", 4, big.NewInt(4), big.NewInt(1)},
+		{"last node in block 1", 4, big.NewInt(7), big.NewInt(1)},
+		{"first node in block 2", 4, big.NewInt(8), big.NewInt(2)},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tree := NewTree(TreeOptions{tt.bfactor, crypto.SHA1})
+			got := tree.blockOfNode(tt.nodeIdx)
+			assert.Equal(t, 0, tt.want.Cmp(got))
+		})
+	}
+}
+
+func TestTree_nodeIndexInBlock(t *testing.T) {
+	tests := []struct {
+		name    string
+		bfactor uint8
+		nodeIdx *big.Int
+		want    *big.Int
+	}{
+		{"first node in block 0", 4, big.NewInt(0), big.NewInt(0)},
+		{"first node in block 1", 4, big.NewInt(4), big.NewInt(0)},
+		{"second node in block 1", 4, big.NewInt(5), big.NewInt(1)},
+		{"last node in block 2", 4, big.NewInt(11), big.NewInt(3)},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tree := NewTree(TreeOptions{tt.bfactor, crypto.SHA1})
+			got := tree.nodeIndexInBlock(tt.nodeIdx)
+			assert.Equal(t, 0, tt.want.Cmp(got))
 		})
 	}
 }
